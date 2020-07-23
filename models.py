1d200b4907bcec766b97064758e96e055da029e5
rita5811 <67811756+rita5811@users.noreply.github.com>
2020-07-23 23:47:27 +0900

    7강

--- models.py
+++ models.py
@@ -1,8 +1,35 @@
 from django.db import models
-from django.views.generic import ListView
-from askcompany.shop.models import Item
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, resolve_url
 
-class ItemListView(ListView):
-    model = Item
-    item_list = ItemListView.as_view()
+def view1(request):
+    return HttpResponse('Hello, Ask Company')
+def view2(request):
+    return render(request, 'template.html')
+def view3(request):
+    return JsonResponse({'hello': 'Ask Company'})
+
+def view1(request):
+return HttpResponseRedirect('/shop/')
+def view2(request):
+    url = resolve_url('shop:item_list')
+    return HttpResponseRedirect(url)
+def view3(request):
+    return redirect('shop:item_list')
+
+def view1(request):
+    try:
+        item = Item.objects.get(pk=100)
+    except Item.DoesNotExist:
+        raise Http404
+def view2(request):
+    item = get_object_or_404(Item, pk=100)
+def view3(request):
+    try:
+        item = Item.objects.get(pk=100)
+    except Item.DoesNotExist:
+        return HttpResponseNotFound()
+
 # Create your models here.
