6a0583e78c9d4b0db58beb45853c41a81acc28af
rita5811 <67811756+rita5811@users.noreply.github.com>
2020-07-24 00:58:36 +0900

    9강

--- models.py
+++ models.py
@@ -1,35 +1,10 @@
 from django.db import models
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, resolve_url
 
-def view1(request):
-    return HttpResponse('Hello, Ask Company')
-def view2(request):
-    return render(request, 'template.html')
-def view3(request):
-    return JsonResponse({'hello': 'Ask Company'})
-
-def view1(request):
-return HttpResponseRedirect('/shop/')
-def view2(request):
-    url = resolve_url('shop:item_list')
-    return HttpResponseRedirect(url)
-def view3(request):
-    return redirect('shop:item_list')
-
-def view1(request):
-    try:
-        item = Item.objects.get(pk=100)
-    except Item.DoesNotExist:
-        raise Http404
-def view2(request):
-    item = get_object_or_404(Item, pk=100)
-def view3(request):
-    try:
-        item = Item.objects.get(pk=100)
-    except Item.DoesNotExist:
-        return HttpResponseNotFound()
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # Create your models here.
