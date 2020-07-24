bec5706b605930a093aa001285e25c9bd7dec41d
rita5811 <67811756+rita5811@users.noreply.github.com>
2020-07-24 23:37:08 +0900

    12강

---
+++ blog/shop/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Item
+# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'short_desc', 'price']
+    list_display_links = ['name']
+    list_filter = ['is_publish']
+    search_fields = ['name']
+
+    def short_desc(self, item):
+        return item.desc[:20]
