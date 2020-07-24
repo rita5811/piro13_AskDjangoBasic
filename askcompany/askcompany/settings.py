9e6b9480cf0b18beb56660b8344da89de82bbdda
rita5811 <67811756+rita5811@users.noreply.github.com>
2020-07-25 03:35:41 +0900

    22-25강

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -130,4 +130,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static')
+]
+
 INTERNAL_IPS = ['127.0.0.1']
